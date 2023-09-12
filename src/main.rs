1eb5e99e9e3725e248c0e60fbe63c97c9db05b27
Rentoo <dreamYiZ@outlook.com>
2023-09-13 03:24:53 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// use rand::seq::SliceRandom;
+use rand::Rng;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("./data").unwrap();
+    let mut rng = rand::thread_rng();
+
+    let vec_paths: Vec<_> = paths.map(|x| x).collect();
+    let mut count = 0;
+
+    let rand_file_index = rng.gen_range(0..vec_paths.len());
+
+
+    for path in vec_paths {
+
+        if count == rand_file_index {
+            let contents = fs::read_to_string(&path.unwrap().path())
+                .expect("Should have been able to read the file");
+
+            let parts: Vec<_> = contents.split("%").skip_while(|&x| x.is_empty()).collect();
+            let mut rng = rand::thread_rng();
+
+            println!("{}", parts[rng.gen_range(0..parts.len())])
+        }
+        count += 1;
+
+    }
 }
